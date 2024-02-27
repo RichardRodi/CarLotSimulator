106dc6b16ab28e5003d829c27d57c01184aecf54
RichardRodi <RichardmRodi@gmail.com>
2024-02-27 09:55:28 -0500

    Finished adding car class with properties

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -15,14 +15,48 @@ namespace CarLotSimulator
 
 
             //Now that the Car class is created we can instanciate 3 new cars
+            var myAudi = new Car();
+
             //Set the properties for each of the cars
+            //Dot Notation
+            myAudi.Year = 2021;
+            myAudi.Make = "Audi";
+            myAudi.Model = "A4";
+            myAudi.EngineNoise = "vroom vroom";
+            myAudi.HonkNoise = "Honk";
+            myAudi.IsDriveable = true;
+
+            //Object Iniitializer Syntax
+            var myMustang = new Car()
+            { 
+            Year = 1967,
+            Make = "Mustang",
+            Model = "Shelby GT 500",
+            EngineNoise = "VROOM!",
+            HonkNoise = "Honk Honk",
+            IsDriveable = true,
+            };
+
+            //Constructor with parameter values
+            var myFirstCar = new Car(1987,"Plymouth","Sundance","Sputter","Quack", false);
+
+
             //Call each of the methods for each car
+            myAudi.MakeEngineNoise();
+            myMustang.MakeEngineNoise();
+            myFirstCar.MakeEngineNoise();
+
+            myAudi.MakeHonkNoise();
+            myMustang.MakeHonkNoise();
+            myFirstCar.MakeHonkNoise();
+
 
             //*************BONUS*************//
 
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            //DONE Set the properties utilizing the 3 different ways we learned about, one way for each car
 
             //*************BONUS X 2*************//
+
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
